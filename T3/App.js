ece48f8347c7df22bf9912ebd2c5e186150c2814
tithi-ashra <168967663+tithi-ashra@users.noreply.github.com>
2025-06-13 13:20:34 +0530

    Add files via upload

---
+++ T3/App.js
@@ -0,0 +1,125 @@
+// import logo from './logo.svg';
+// import i1 from './imag1.jpg';
+// import i2 from './imag2.jpg';
+// import i3 from './imag3.jpg';
+import './App.css';
+// import T1Uc1 from './T1Uc1';
+// import Ue1 from './Ue1';
+// import Ue2 from './Ue2';
+// import Pr1 from './pr1';
+// import Task1 from './Task1';
+// import Task2 from './Task2';
+// import Task3 from './Task3';
+// import Task4 from './Task4';
+// import Task5 from './Task5';
+// import Task6 from './Task6';
+// import Task7 from './Task7';
+// import Task8 from './Task8';
+// import Task9 from './Task9';
+// import Main from './components/main';
+// import Us1 from './us1';
+// import Us2 from './Us2';
+// import Us3 from './Us3';
+// import Us4 from './Us4';
+// import Us5 from './Us5';
+// import Us6 from './Us6';
+// import Us7 from './Us7';
+// import Us8 from './Us8';
+// import Us9 from './Us9';
+// import Ur1 from './Ur1';
+// import Ur2 from './Ur2';
+// import Ur3 from './Ur3';
+// import Main from './main';
+// import Comp3 from './Comp3';
+// import Uc2 from './Uc2';
+import axios1 from './axios1'
+
+
+function App() {
+  // const item = 'mobile';
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    // <>    
+    //   <h1>Helloo from app.js</h1>
+    //   <Pr1 />
+    //   <Task1 />
+    //   <Task2/>
+    // </>
+
+    // <>
+    //   <Task3 name='laptop' price='50000'/>
+    //   <Task3 name='TV' price='50000'/>
+    //   <Task3 name={item} price='50000'/>
+    //   <Task4 img={i1} name='Mobile' price='10000' desc="phone" />
+    //   <Task4 img={i2} name='Mobile' price='100000' desc="phone" />
+    //   <Task4 img={i3} name='Mobile' price='20000' desc="phone" />
+    // </>
+
+    // <>
+    //   <Task5 />
+    //   <Task6 />
+    //   <Task7 />
+    //   <Task8 item={data} />
+    //   <Task9 />
+    // </>
+
+    // <>
+    //   <Main />
+    // </>
+
+    // <>
+    //   <Us1 />
+    //   <Us2 />
+    //   <Us3 />
+    //   <Us4 />
+    //   <Us5 />
+    //   <Us6 />
+    //   <Us7 />
+    //   <Us8 />
+    //   <Us9 />
+    // </>
+
+    // <>
+    //   <Ur1 />
+    //   <Ur2 />
+    //   <Ur3 />
+    // </>
+
+    // <>
+    //   <Main />
+    //   <Uc2 />
+    //   <Comp3 />
+    // </>
+
+    // <>
+    // <T1Uc1 />
+    // </>
+
+    // <>
+    //   <Ue1 />
+    //   <Ue2 />
+    // </>
+
+    <>
+      {/* <axios1 /> */}
+    </>
+  );
+};
+
+export default App;
